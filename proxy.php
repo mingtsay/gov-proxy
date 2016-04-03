9e31b0ec8d12bf96918b28033a29f95512349cb6
Ronny Wang <ronnywang@gmail.com>
2016-04-03 18:19:29 +0800

    add cookie support in some host

--- proxy.php
+++ proxy.php
@@ -18,8 +18,39 @@ if (0 === strpos($url, 'http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx')) {
     $referer = $url;
 }
 
+$allow_cookies = array();
+if ('civil.kcg.gov.tw' == $url_parts['host']) {
+    $allow_cookies[] = 'ASP.NET_SessionId';
+}
+
+$headers = array();
+$headers[] = "X-Forwarded-For: {$_SERVER['REMOTE_ADDR']}";
+
+$cookies = array();
+foreach (explode('; ', $_SERVER['HTTP_COOKIE']) as $term) {
+    list($key, $value) = explode('=', $term, 2);
+    $cookies[urldecode($key)] = urldecode($value);
+}
+
+foreach ($allow_cookies as $key) {
+    if (array_key_exists($url_parts['host'] . ':' . $key, $cookies)) {
+        $headers[] = 'Cookie: ' . urlencode($key) . '=' . urlencode($cookies[$key]);
+    }
+}
+
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_REFERER, $referer);
 curl_setopt($curl, CURLOPT_USERAGENT, $agent);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-Forwarded-For: {$_SERVER['REMOTE_ADDR']}"));
-curl_exec($curl);
+curl_setopt($curl, CURLOPT_HEADER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$content = curl_exec($curl);
+list($header, $body) = explode("\r\n\r\n", $content, 2);
+foreach (explode("\r\n", $header) as $header_line) {
+    if (preg_match('#^Set-Cookie: ([^=]*)=(.*)#', $header_line, $matches)) {
+        if (in_array($matches[1], $allow_cookies)) {
+            header("Set-Cookie: {$url_parts['host']}:{$matches[1]}={$matches[2]}");
+        }
+    }
+}
+echo $body;
